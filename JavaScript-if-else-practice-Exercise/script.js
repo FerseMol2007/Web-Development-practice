acddc16305c0c1d158db5e72a28adf81536cb5f8
Ferse Mol M D <fersemolmd@gmail.com>
2026-08-14 19:53:45 +0530

    Add files via upload

---
+++ JavaScript-if-else-practice-Exercise/script.js
@@ -0,0 +1,17 @@
+var season="fall"
+if(season=="spring")
+{
+    console.log("Enjoy the blooming flowers")
+}    
+if(season=="Summer")
+{
+    console.log("have fun in the sun")
+}    
+if(season=="Autumn"||"fall")
+{
+    console.log("Admire the colorful leaves")
+}    
+if(season=="Winter")
+{
+    console.log("Bundle up and stay warm")
+}    
